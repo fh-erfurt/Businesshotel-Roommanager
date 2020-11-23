300897531c7d56bb61e331ab38cffb8fc30d5bfe
Thom-Mon <thomas.gebel@fh-erfurt.de>
2020-11-23 11:16:46 +0100

    Booking um einige Variablen ergänzt.

--- src/main/java/de/fourofakind/businesshotel/Booking.java
+++ src/main/java/de/fourofakind/businesshotel/Booking.java
@@ -1,4 +1,23 @@
 package de.fourofakind.businesshotel;
 
 public class Booking {
+
+
+
+
+
+    private string startDate;
+    private string endDate;
+    private string bookingDate;
+    private string roomCategory;
+    private string specialWishes;
+
+    private float pricing;
+    private int empNo;
+
+    private boolean isBusinessCustomer;
+
+
+
+
 }
