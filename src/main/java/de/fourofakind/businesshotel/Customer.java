5e7587807dfedba81e95a8e1baeba68fc0089329
nikschen <niklaswiemuth@gmail.com>
2021-01-07 14:10:20 +0100

    AutoFormat

--- src/main/java/de/fourofakind/businesshotel/Customer.java
+++ src/main/java/de/fourofakind/businesshotel/Customer.java
@@ -3,46 +3,56 @@ package de.fourofakind.businesshotel;
 import static de.fourofakind.businesshotel.StartingClass.*;
 
 /**
-* customer class manages every methode done by the customer and its personal data
-*/
+ * customer class manages every methode done by the customer and its personal data
+ */
 
-public class Customer {
+public class Customer
+{
 
-    enum paymentMethods {
+    enum paymentMethods
+    {
         debit, paypal, bill
     }
 
-    public Customer(int customerID, ContactDetails contactDetails, paymentMethods paymentMethod) {
+    public Customer (int customerID, ContactDetails contactDetails, paymentMethods paymentMethod)
+    {
         this.customerID = customerID;
         this.contactDetails = contactDetails;
         changePaymentMethod(paymentMethod);
     }
 
-    public int getCustomerID() {
+    public int getCustomerID ()
+    {
         return this.customerID;
     }
-    public paymentMethods getPaymentMethod() {
+
+    public paymentMethods getPaymentMethod ()
+    {
         return this.paymentMethod;
     }
 
-    public void setCustomerID(Integer customerID) {
+    public void setCustomerID (Integer customerID)
+    {
         this.customerID = customerID;
     }
 
     /**
      * <p> generates new bookingRequest and add ist to bookingRequests list
      * </p>
-     * @param dateFrame for date of booking
      *
+     * @param dateFrame for date of booking
      */
 
-    public void sendBookingRequest(DateFrame dateFrame) {
+    public void sendBookingRequest (DateFrame dateFrame)
+    {
         // setup database request here
         int customerID = this.customerID;
         BookingRequest newRequest = new BookingRequest(customerID, dateFrame);
         bookingRequests.add(newRequest);
     }
-    public void changePaymentMethod(paymentMethods paymentMethod) {
+
+    public void changePaymentMethod (paymentMethods paymentMethod)
+    {
         this.paymentMethod = paymentMethod;
     }
 
