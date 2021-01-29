fa1bf9f7a10633e237ce2e445e46e0e8541dfa72
nikschen <niklaswiemuth@gmail.com>
2021-01-29 10:04:26 +0100

    setter entfernt und final gesetzt

--- src/main/java/de/fourofakind/businesshotel/common/Role.java
+++ src/main/java/de/fourofakind/businesshotel/common/Role.java
@@ -18,19 +18,9 @@ public class Role
         return isEnabledToManageRooms;
     }
 
-    public void setEnabledToManageRooms (boolean enabledToManageRooms)
-    {
-        isEnabledToManageRooms = enabledToManageRooms;
-    }
-
     public boolean isEnabledToManageBookings ()
     {
         return isEnabledToManageBookings;
-    }
-
-    public void setEnabledToManageBookings (boolean enabledToManageBookings)
-    {
-        isEnabledToManageBookings = enabledToManageBookings;
     }
 
     public boolean isEnabledToManageCustomerData ()
@@ -38,12 +28,9 @@ public class Role
         return isEnabledToManageCustomerData;
     }
 
-    public void setEnabledToManageCustomerData (boolean enabledToManageCustomerData)
-    {
-        isEnabledToManageCustomerData = enabledToManageCustomerData;
-    }
 
-    private boolean isEnabledToManageRooms;
-    private boolean isEnabledToManageBookings;
-    private boolean isEnabledToManageCustomerData;
+
+    private final boolean isEnabledToManageRooms;
+    private final boolean isEnabledToManageBookings;
+    private final boolean isEnabledToManageCustomerData;
 }
