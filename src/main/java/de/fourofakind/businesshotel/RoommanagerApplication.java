83784293a0d2ea8ec7d508d4d013ebf76542da0a
nikschen <niklaswiemuth@gmail.com>
2021-05-15 14:26:02 +0200

    #22 Grund-Umbau zu Spring Projekt (Deklaration für Entities fehlen noch)

---
+++ src/main/java/de/fourofakind/businesshotel/RoommanagerApplication.java
@@ -0,0 +1,13 @@
+package de.fourofakind.businesshotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoommanagerApplication
+{
+    public static void main (String[] args)
+    {
+        SpringApplication.run(RoommanagerApplication.class,args);
+    }
+}
