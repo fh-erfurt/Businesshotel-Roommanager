977bd937f6711c3da11ab77e5c18a0395c8de2e1
nikschen <niklaswiemuth@gmail.com>
2021-06-01 11:06:51 +0200

    database

--- src/main/java/de/fourofakind/businesshotel/RoommanagerApplication.java
+++ src/main/java/de/fourofakind/businesshotel/RoommanagerApplication.java
@@ -1,8 +1,10 @@
 package de.fourofakind.businesshotel;
 
 
+import de.fourofakind.businesshotel.entities.common.Role;
 import de.fourofakind.businesshotel.entities.employees.Employee;
 import de.fourofakind.businesshotel.repositories.EmployeeRepository;
+import de.fourofakind.businesshotel.repositories.RoleRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,11 +20,13 @@ public class RoommanagerApplication
     }
 
     @Bean
-    CommandLineRunner commandLineRunner (EmployeeRepository employeeRepository)
+    CommandLineRunner commandLineRunner (EmployeeRepository employeeRepository, RoleRepository roleRepository)
     {
         return args->{
-            Employee MaxMustermann=new Employee("Max Mustermann");
-            employeeRepository.save(MaxMustermann);
+//            Employee MaxMustermann=new Employee("Max Mustermann");
+//            employeeRepository.save(MaxMustermann);
+            Role Babo = new Role("Babo",true, true, true);
+            roleRepository.save(Babo);
         };
     }
 }
