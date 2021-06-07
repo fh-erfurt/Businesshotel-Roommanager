f0454582817665c60985ecf2a1f175ee6e70e0ea
nikschen <niklaswiemuth@gmail.com>
2021-06-07 18:49:42 +0200

    Accountdetails hinzugefügt

---
+++ src/main/java/de/fourofakind/businesshotel/entities/common/AccountDetails.java
@@ -0,0 +1,23 @@
+package de.fourofakind.businesshotel.entities.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity(name="AccountDetails")
+@Table(name="account_details")
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class AccountDetails
+{
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    Integer accountID;
+    String username;
+    String passwordHash;
+}
