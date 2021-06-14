708f980d5a16552c5a32a12fdfb4540a21ce29ea
Thom-Mon <thomas.gebel@fh-erfurt.de>
2021-06-14 15:27:00 +0200

    GUI Working Version (ohne Anbindung) #38

--- src/main/java/de/fourofakind/businesshotel/client/angularclient/src/app/home/home.component.ts
+++ src/main/java/de/fourofakind/businesshotel/client/angularclient/src/app/home/home.component.ts
@@ -1,23 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  products = [];
+  constructor() { }
 
-  constructor(private dataService: DataService) { }
-
-  ngOnInit() {
-
-    this.dataService.sendGetRequest().subscribe((data: any[])=>{
-      console.log(data);
-      this.products = data;
-    })  
+  ngOnInit(): void {
   }
 
 }
