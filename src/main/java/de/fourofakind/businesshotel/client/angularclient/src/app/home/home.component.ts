ff7dec5891bcd12ca8d94523e7be6eedeff7d39a
nikschen <niklaswiemuth@gmail.com>
2021-06-14 16:03:41 +0200

    keine Ahnung was passiert ist :D

--- src/main/java/de/fourofakind/businesshotel/client/angularclient/src/app/home/home.component.ts
+++ src/main/java/de/fourofakind/businesshotel/client/angularclient/src/app/home/home.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  products = [];
 
-  ngOnInit(): void {
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+
+    this.dataService.sendGetRequest().subscribe((data: any)=>{
+      console.log(data);
+      this.products = data;
+    })
   }
 
 }
