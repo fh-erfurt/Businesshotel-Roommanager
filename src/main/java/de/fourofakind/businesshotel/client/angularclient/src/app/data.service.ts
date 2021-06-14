45d3085e17f838342ce3341712a5431349e9ebc2
nikschen <niklaswiemuth@gmail.com>
2021-06-14 17:00:05 +0200

    serverport angegeben, damit nichts behindert

--- src/main/java/de/fourofakind/businesshotel/client/angularclient/src/app/data.service.ts
+++ src/main/java/de/fourofakind/businesshotel/client/angularclient/src/app/data.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DataService {
 
-  private REST_API_SERVER = "http://localhost:3000";
+  private REST_API_SERVER = "http://localhost:8081/employee";
 
   constructor(private httpClient: HttpClient) { }
 
