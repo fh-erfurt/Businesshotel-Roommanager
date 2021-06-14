0263922f737db3295ab6be1412c2adac03753824
Thom-Mon <thomas.gebel@fh-erfurt.de>
2021-06-14 21:44:05 +0200

    Angular RestApi anbindung für JSON DB abgeschlossen

--- src/main/java/de/fourofakind/businesshotel/client/angularclient/src/app/home/home.component.ts
+++ src/main/java/de/fourofakind/businesshotel/client/angularclient/src/app/home/home.component.ts
@@ -1,23 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataService } from '../data.service';
+import {  takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { HttpResponse } from '@angular/common/http';
+
+import { Product } from '../product';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  products = [];
+  products: Product[] = [];
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
   constructor(private dataService: DataService) { }
 
-  ngOnInit() {
+  ngOnInit() {//umstellung auf Httpresponse<Product[] und schauen was der fehler dort ist>
 
-    this.dataService.sendGetRequest().subscribe((data: any)=>{
-      console.log(data);
-      this.products = data;
-    })
+    this.dataService.sendGetRequest().pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<any>)=>{
+      console.log(res);
+      this.products = res.body;
+    })  
+  }
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    // Unsubscribe from the subject
+    this.destroy$.unsubscribe();
   }
 
+  public firstPage() {
+    this.products = [];
+    this.dataService.sendGetRequestToUrl(this.dataService.first).pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<any>) => {
+      console.log(res);
+      this.products = res.body;
+    })
+  }
+  public previousPage() {
+
+    if (this.dataService.prev !== undefined && this.dataService.prev !== '') {
+      this.products = [];
+      this.dataService.sendGetRequestToUrl(this.dataService.prev).pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<any>) => {
+        console.log(res);
+        this.products = res.body;
+      })
+    }
+
+  }
+  public nextPage() {
+    if (this.dataService.next !== undefined && this.dataService.next !== '') {
+      this.products = [];
+      this.dataService.sendGetRequestToUrl(this.dataService.next).pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<any>) => {
+        console.log(res);
+        this.products = res.body;
+      })
+    }
+  }
+  public lastPage() {
+    this.products = [];
+    this.dataService.sendGetRequestToUrl(this.dataService.last).pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<any>) => {
+      console.log(res);
+      this.products = res.body;
+    })
+  }
 }
+
+
+
+
