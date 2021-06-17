ec53424feef2e70e1c94c7a577d2d92f292ba76b
Marius Lange <mariuslange2000@gmail.com>
2021-06-17 18:15:16 +0200

    Components + Pfad müssen in app-routing.module.ts hinterlegt werden

---
+++ src/main/java/de/fourofakind/businesshotel/client/angularclient2ndTry/src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from '../login/login.component';
+import { RoomComponent } from '../room/room.component';
+import { DashboardComponent } from '../dashboard/dashboard.component'
+import { MainComponent } from '../main/main.component'
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'rooms',
+    component: RoomComponent
+  },
+
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
+})
+export class AppRoutingModule { }
--- src/main/java/de/fourofakind/businesshotel/client/angularclient2ndTry/src/app/app.module.ts
+++ src/main/java/de/fourofakind/businesshotel/client/angularclient2ndTry/src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { RoomComponent } from './room/room.component';
-
 import { HttpClientModule } from '@angular/common/http';
+
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { AppComponent } from './app.component';
+
+import { RoomComponent } from './room/room.component';
 import { LoginComponent } from './login/login.component';
-
-
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MainComponent } from './main/main.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RoomComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent,
+    MainComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
