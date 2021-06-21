650029581efc1b76dbf5e98d69949e7ce48b325a
nikschen <niklaswiemuth@gmail.com>
2021-06-21 13:00:28 +0200

    Custom Implementierung, um customer nach vor und nachnamen suchen zu können

--- src/main/java/de/fourofakind/businesshotel/server/repositories/CustomerRepository.java
+++ src/main/java/de/fourofakind/businesshotel/server/repositories/CustomerRepository.java
@@ -1,13 +1,13 @@
 package de.fourofakind.businesshotel.server.repositories;
 
+
 import de.fourofakind.businesshotel.server.entities.customers.Customer;
+import de.fourofakind.businesshotel.server.repositories.custom.CustomerRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.stereotype.Repository;
 
 @RepositoryRestResource(collectionResourceRel = "customer", path="customer")
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer,Integer>
+public interface CustomerRepository extends JpaRepository<Customer,Integer>, CustomerRepositoryCustom
 {
 
 }
