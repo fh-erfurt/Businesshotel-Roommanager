25918ee5ea51dcb2e52d205253155b9fc6402352
nikschen <niklaswiemuth@gmail.com>
2021-06-22 16:24:07 +0200

    angular client wieder in regulären ordner verschoben

--- src/main/java/de/fourofakind/businesshotel/client/karma.conf.js
+++ src/main/java/de/fourofakind/businesshotel/client/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angularclient2ndTry'),
+      dir: require('path').join(__dirname, './coverage/bh-room_client'),
       subdir: '.',
       reporters: [
         { type: 'html' },
