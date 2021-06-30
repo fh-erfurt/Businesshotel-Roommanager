bcf6a43eeba2e4a1dc6baa72b25dd6b8a005488f
Thom-Mon <thomas.gebel@fh-erfurt.de>
2021-06-30 17:46:08 +0200

    Employee-Management angefügt - noch ohne Logik

--- src/main/java/de/fourofakind/businesshotel/client/src/app/components/employee-management/employee-management.component.ts
+++ src/main/java/de/fourofakind/businesshotel/client/src/app/components/employee-management/employee-management.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from "@angular/material/core";
+import {MAT_MOMENT_DATE_ADAPTER_OPTIONS, MomentDateAdapter} from "@angular/material-moment-adapter";
 
 @Component({
   selector: 'app-employee-management',
@@ -6,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-management.component.scss']
 })
 export class EmployeeManagementComponent implements OnInit {
-
+  employee=
+    {
+      employeeID:"",
+    };
   constructor() { }
 
   ngOnInit(): void {
