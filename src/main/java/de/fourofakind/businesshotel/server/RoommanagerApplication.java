b3bd680435f3b0065ad852a1e7210562a898f49b
nikschen <niklaswiemuth@gmail.com>
2021-07-02 09:58:18 +0200

    Ids werden nun ausgespuckt

--- src/main/java/de/fourofakind/businesshotel/server/RoommanagerApplication.java
+++ src/main/java/de/fourofakind/businesshotel/server/RoommanagerApplication.java
@@ -1,6 +1,17 @@
 package de.fourofakind.businesshotel.server;
 
 
+import de.fourofakind.businesshotel.server.entities.bookings.Booking;
+import de.fourofakind.businesshotel.server.entities.bookings.ConferenceRoomBooking;
+import de.fourofakind.businesshotel.server.entities.bookings.HotelRoomBooking;
+import de.fourofakind.businesshotel.server.entities.common.AccountDetails;
+import de.fourofakind.businesshotel.server.entities.customers.BookingRequest;
+import de.fourofakind.businesshotel.server.entities.customers.ContactData;
+import de.fourofakind.businesshotel.server.entities.customers.Customer;
+import de.fourofakind.businesshotel.server.entities.employees.Employee;
+import de.fourofakind.businesshotel.server.entities.rooms.ConferenceRoom;
+import de.fourofakind.businesshotel.server.entities.rooms.HotelRoom;
+import de.fourofakind.businesshotel.server.entities.rooms.Room;
 import de.fourofakind.businesshotel.server.repositories.AccountDetailsRepository;
 import de.fourofakind.businesshotel.server.repositories.EmployeeRepository;
 import de.fourofakind.businesshotel.server.repositories.RoleRepository;
@@ -8,6 +19,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
 
 @SpringBootApplication
@@ -16,6 +28,15 @@ public class RoommanagerApplication
     public static void main (String[] args)
     {
         SpringApplication.run(RoommanagerApplication.class,args);
+    }
+
+    @Bean
+    public RepositoryRestConfigurer repositoryRestConfigurer ()
+    {
+        return RepositoryRestConfigurer.withConfig(config -> {
+            config.exposeIdsFor(Booking.class, ConferenceRoomBooking.class, HotelRoomBooking.class, AccountDetails.class, BookingRequest.class,ContactData.class,
+                                Customer.class, Employee.class, Room.class, HotelRoom.class, ConferenceRoom.class);
+        });
     }
 
     @Bean
