536806bc9bcbb0fd321a92f19695b36001c926ea
nikschen <niklaswiemuth@gmail.com>
2021-07-27 14:21:16 +0200

    Funktionen zum Löschen und Erstellen von Mitarbeitern angelegt

--- src/main/java/de/fourofakind/businesshotel/client/src/app/services/accountdetails/accountdetails.service.ts
+++ src/main/java/de/fourofakind/businesshotel/client/src/app/services/accountdetails/accountdetails.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Accountdetails} from "./accountdetails";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountdetailsService {
 
-  constructor() { }
+  private readonly baseUrl:string;
+  constructor(private http: HttpClient) {
+    this.baseUrl="http://localhost:8081/accountdetails/";
+  }
+
+  public save(accountdetails: Accountdetails) {
+    console.log(accountdetails);
+
+    this.http.post<Accountdetails>(this.baseUrl, accountdetails)
+      .subscribe(
+        (val)=>
+        {
+          console.log("Post call => successful value returned in body: ", val);
+        },
+
+        response=>
+        {
+          console.log("Post call => error in: ", response);
+        },
+        ()=>
+        {
+          console.log("Post call => Booking creation successful");
+        }
+      )
+  }
 }
--- src/main/java/de/fourofakind/businesshotel/client/src/app/services/employee/employee.service.ts
+++ src/main/java/de/fourofakind/businesshotel/client/src/app/services/employee/employee.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { RawData } from './employee';
+import {Employee, RawData} from './employee';
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,56 @@ export class EmployeeService {
 
   public getEmployees(): Observable<RawData>{
     return this.http.get<RawData>(`${this.baseUrl}`)
+  }
+
+  public getEmployee(id:number): Observable<Employee>
+  {
+    return this.http.get<Employee>(`${this.baseUrl}${id}`).pipe(
+      map((result:any) =>{
+        //console.log(result);
+        return result;
+      })
+    )
+  }
+
+  public save(employee: Employee) {
+    console.log(employee);
+
+    this.http.post<Employee>(this.baseUrl, employee)
+      .subscribe(
+        (val)=>
+        {
+          console.log("Post call => successful value returned in body: ", val);
+        },
+
+        response=>
+        {
+          console.log("Post call => error in: ", response);
+        },
+        ()=>
+        {
+          console.log("Post call => Employee creation successful");
+        }
+      )
+  }
+
+  public delete(id: number) {
+    this.http.delete<Employee>(`${this.baseUrl}${id}`)
+      .subscribe(
+        (val)=>
+        {
+          console.log("Post call => successful value returned in body: ", val);
+        },
+
+        response=>
+        {
+          console.log("Post call => error in: ", response);
+        },
+        ()=>
+        {
+          console.log("Post call => Employee deletion successful");
+        }
+      )
   }
 }
 
