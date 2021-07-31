1ca644763635ba14a8b90fd184766dd90274e048
nikschen <niklaswiemuth@gmail.com>
2021-07-31 19:14:08 +0200

    DELETEDUSER in DB eingepflegt

--- database/Testdatensatz.sql
+++ database/Testdatensatz.sql
@@ -1,22 +1,24 @@
 
 
-INSERT INTO `contact_data` (`contact_data_id`, `first_name`, `last_name`, `street_name`, `street_number`, `postal_code`, `city_name`, `phone`, `mail_address`, `payment_credentials`, `updated_at`, `created_at`) VALUES
-(1, 'Koji', 'Kondo', 'Bergstraße', '14', '07749', 'Jena', '0900767676', 'koj_kondo@web.de','koj_kondo@web.de', NULL, '2021-05-24 08:21:37'),
-(2, 'Hiroshi', 'Yamauchi', 'Zauberweg', '9', '141815', 'Muisin', '+81142656565', 'suzukiDestroyer@jin.jp','DE1234567891011121', NULL, '2021-05-24 08:23:39'),
-(3, 'Manfred', 'Reide', 'Chemietischstraße', '1', '142857', 'Simcity', '0124-148257', 'fensterWarenZuTeuer@dunkeldrin.de', NULL, NULL, '2021-05-24 08:25:26'),
-(4, 'Jonas', 'Stenberg', 'Gewinnerstraße', '7', '01248', 'Bergen', '14191111-142', 'BergeLiebhabber@gmx.net',NULL, NULL, '2021-05-24 08:29:04'),
-(5, 'Shigeru', 'Miyamoto', 'Nintendoweg', '14', '00100', 'Phinshin', '+81142833341', 'MarioMaker@shin.jp','BergeLiebhabber@gmx.net', NULL, '2021-05-24 08:30:17');
+INSERT INTO `contact_data` (`first_name`, `last_name`, `street_name`, `street_number`, `postal_code`, `city_name`, `phone`, `mail_address`, `payment_credentials`, `updated_at`, `created_at`) VALUES
+('DELETEDUSER', 'DELETEDUSER', 'DELETEDUSER', 'DELETED', 'DELETED', 'DELETEDUSER', 'DELETEDUSER', 'DELETEDUSER','DELETEDUSER', NULL, '2021-05-24 08:21:37'),
+('Koji', 'Kondo', 'Bergstraße', '14', '07749', 'Jena', '0900767676', 'koj_kondo@web.de','koj_kondo@web.de', NULL, '2021-05-24 08:21:37'),
+('Hiroshi', 'Yamauchi', 'Zauberweg', '9', '141815', 'Muisin', '+81142656565', 'suzukiDestroyer@jin.jp','DE1234567891011121', NULL, '2021-05-24 08:23:39'),
+('Manfred', 'Reide', 'Chemietischstraße', '1', '142857', 'Simcity', '0124-148257', 'fensterWarenZuTeuer@dunkeldrin.de', NULL, NULL, '2021-05-24 08:25:26'),
+('Jonas', 'Stenberg', 'Gewinnerstraße', '7', '01248', 'Bergen', '14191111-142', 'BergeLiebhabber@gmx.net',NULL, NULL, '2021-05-24 08:29:04'),
+('Shigeru', 'Miyamoto', 'Nintendoweg', '14', '00100', 'Phinshin', '+81142833341', 'MarioMaker@shin.jp','BergeLiebhabber@gmx.net', NULL, '2021-05-24 08:30:17');
 
-INSERT INTO `account_details` (`account_id`, `username`, `password_hash`, `created_at`, `updated_at`) VALUES
-(1, 'IggyPop', 'password123', '2021-05-31 10:27:36', NULL),
-(2, 'SchlaubiSchlumpf', 'schlumpfineGeschlumpft', '2021-05-31 10:27:36', NULL),
-(3, 'Brightwing', 'ItOnlyHurtsUntilYouDie', '2021-05-31 10:28:18', NULL),
-(4, 'Kekslieferant400', 'MjamMjamMjam?!', '2021-05-31 10:28:48', NULL),
-(5, 'RocknRollOpa', 'ThePolice_123', '2021-05-31 10:29:16', NULL),
-(7, 'FlodinWiesret', '66410', '2021-05-31 10:31:18', NULL),
-(8, 'MagnolienAusStahl', 'Lemur418', '2021-05-31 10:31:38', NULL),
-(9, 'ColumboAuge', 'katalimata_megagut', '2021-05-31 10:32:04', NULL),
-(10, 'Marius Mac Mac', 'LangeIstsHer', '2021-05-31 10:32:48', NULL);
+INSERT INTO `account_details` (`username`, `password_hash`, `created_at`, `updated_at`) VALUES
+('DELETEDUSER', 'DELETEDUSER', '2021-05-31 10:27:36', NULL),
+('IggyPop', 'password123', '2021-05-31 10:27:36', NULL),
+('SchlaubiSchlumpf', 'schlumpfineGeschlumpft', '2021-05-31 10:27:36', NULL),
+('Brightwing', 'ItOnlyHurtsUntilYouDie', '2021-05-31 10:28:18', NULL),
+('Kekslieferant400', 'MjamMjamMjam?!', '2021-05-31 10:28:48', NULL),
+('RocknRollOpa', 'ThePolice_123', '2021-05-31 10:29:16', NULL),
+('FlodinWiesret', '66410', '2021-05-31 10:31:18', NULL),
+('MagnolienAusStahl', 'Lemur418', '2021-05-31 10:31:38', NULL),
+('ColumboAuge', 'katalimata_megagut', '2021-05-31 10:32:04', NULL),
+('Marius Mac Mac', 'LangeIstsHer', '2021-05-31 10:32:48', NULL);
 
 INSERT INTO `role` (`role_name`, `is_enabled_to_manage_rooms`, `is_enabled_to_manage_bookings`, `is_enabled_to_manage_customer_data`, `is_enabled_to_manage_employee_data`, `created_at`, `updated_at`) VALUES
 ('Buchungsmanager', 0, 1, 0, 0, '2021-05-31 10:52:45', 'NULL ON UPDATE CURRENT_TIMESTAMP'),
@@ -57,12 +59,14 @@ INSERT INTO `room` (`room_no`, `area_in_sqr_metre`, `category`, `price_per_unit`
 (7, 50, 'BIGGROUP',     '35', NULL, '2021-05-24 06:01:55',NULL,7,'CONFERENCEROOM');
 
 
-INSERT INTO `customer` (`customer_id`, `payment_method`, `updated_at`, `created_at`, `contact_data_id`,`account_id`, `is_business_customer`) VALUES
-(NULL, 'paypal', NULL, '2021-05-24 08:33:41', 1,1,true),
-(NULL, 'bill', '2021-05-24 08:34:52', '2021-05-24 08:34:18', 4,2,false),
-(NULL, 'debit', NULL, '2021-05-24 08:35:39', 2,3,true),
-(NULL, 'bill', NULL, '2021-05-24 08:35:56', 3,4,false),
-(NULL, 'paypal', NULL, '2021-05-24 08:36:25', 5,5,true);
+INSERT INTO `customer` (`payment_method`, `updated_at`, `created_at`, `contact_data_id`,`account_id`, `is_business_customer`) VALUES
+(NULL, NULL, '2021-05-24 08:33:41', 1,1,false),
+('paypal', NULL, '2021-05-24 08:33:41', 2,2,true),
+('bill', '2021-05-24 08:34:52', '2021-05-24 08:34:18', 3,3,false),
+('debit', NULL, '2021-05-24 08:35:39', 4,4,true),
+('bill', NULL, '2021-05-24 08:35:56', 5,5,false),
+('paypal', NULL, '2021-05-24 08:36:25', 6,7,true),
+('paypal', NULL, '2021-05-24 08:36:25', 6,8,true);
 
 INSERT INTO `booking` (`booking_no`, `updated_at`, `room_no`, `booking_type`, `start_date`, `end_date`, `special_wishes`, `emp_no`, `customer_id`, `pricing`) VALUES
 (1, '2021-05-24 06:42:26',  2, 'ConferenceRoomBooking', '2021-04-01 10:30:00', '2021-04-04 10:30', 'Cola so viel und so oft wie geht', 4, 2,1111.99),
