41857ffcbd19689a3c5591934dea383cf0232229
nikschen <niklaswiemuth@gmail.com>
2021-08-15 14:45:11 +0200

    Methode auf Java Seite zur Filterung von Buchungen, welceh innerhalb eines Zeitraums beginnen, enden oder darin liegen (oder anders überschneiden)

--- src/main/java/de/fourofakind/businesshotel/server/repositories/BookingRepository.java
+++ src/main/java/de/fourofakind/businesshotel/server/repositories/BookingRepository.java
@@ -4,8 +4,10 @@ import de.fourofakind.businesshotel.server.entities.bookings.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import java.util.Date;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
@@ -15,4 +17,11 @@ public interface BookingRepository extends JpaRepository<Booking,Integer>
     List<Booking> findByCustomerID (@Param("customerID") Integer customerID);
     List<Booking> findByEmpNo (@Param("empNo") Integer empNo);
     List<Booking> findByRoomNo (@Param("roomNo") Integer roomNo);
+    List<Booking> findByStartDateIsBetweenOrEndDateIsBetweenOrStartDateBeforeAndEndDateAfter
+            (@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)@Param("startDate") Date startDateFirstCondition,
+             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)@Param("endDate") Date endDateFirstCondition,
+             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)@Param("startDate") Date startDateSecondCondition,
+             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)@Param("endDate") Date endDateSecondCondition,
+             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)@Param("startDate") Date startDateThirdCondition,
+             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)@Param("endDate") Date endDateThirdCondition);
 }
