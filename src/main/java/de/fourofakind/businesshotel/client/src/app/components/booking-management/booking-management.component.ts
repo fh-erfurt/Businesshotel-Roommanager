088e9c483005d2f1ae6f9c3b80e67048c7a9f8ac
nikschen <niklaswiemuth@gmail.com>
2021-08-19 08:00:57 +0200

    fertig kommentiert

--- src/main/java/de/fourofakind/businesshotel/client/src/app/components/booking-management/booking-management.component.ts
+++ src/main/java/de/fourofakind/businesshotel/client/src/app/components/booking-management/booking-management.component.ts
@@ -23,10 +23,11 @@ import {RoleService} from "../../services/role/role.service";
       deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
     },
 
-    {provide: MAT_DATE_FORMATS,
+    {
+      provide: MAT_DATE_FORMATS,
       useValue: {
         parse: {
-          dateInput: ['LL', 'DD.MM.YYYY' ],
+          dateInput: ['LL', 'DD.MM.YYYY'],
         },
         display: {
           dateInput: 'DD-MM-YYYY',
@@ -36,74 +37,79 @@ import {RoleService} from "../../services/role/role.service";
   ],
 })
 
-
+/*
+* Component for Management (Add, Update, Get, Delete) of Bookings
+* consumes form data and calls corresponding services
+*/
 export class BookingManagementComponent implements OnInit {
 
-  constructor(private bookingService: BookingService,
-              private roomService: RoomService,
-              private roleService: RoleService)
-  {}
-
-  ngOnInit()
-  {
-
-  }
-
-
-  private readonly department:string="booking-management";
-
-  //formdata
-  booking!: Booking;
+  //form data
   bookingNo!: number;
   bookingType!: string;
-  bookings: Booking[] = [];
-  calculatedPricing!: number;
   customerID!: number;
   endDate!: string;
   endTime!: string;
+  roomNo!: number;
+  specialWishes!: string;
+  startDate!: string;
+  startTime!: string;
+
+  //helper variables
+  booking!: Booking;
+  bookings: Booking[] = [];
+  calculatedPricing!: number;
   endTimestamp!: Date;
   foundBooking!: Booking | null;
   isChecked: boolean = false;
   occupationChecked: boolean = false;
   pricePerUnit!: number;
-  roomNo!: number;
   rooms!: Room[];
-  specialWishes!: string;
-  startDate!: string;
-  startTime!: string;
   startTimestamp!: Date;
   today: Date = new Date();
 
-  //errors
-  startDateError:boolean=false;
-  endDateInPastError:boolean=false;
-  endDateBeforeStartDateError:boolean=false;
+  //custom errors
+  startDateError: boolean = false;
+  endDateInPastError: boolean = false;
+  endDateBeforeStartDateError: boolean = false;
+  alerts: Alert[] = [];
 
+  private readonly department: string = "booking-management";
 
-  alerts:Alert[]=[];
+  constructor(private bookingService: BookingService,
+              private roomService: RoomService,
+              private roleService: RoleService) {
+  }
 
+  ngOnInit() {
+
+  }
+
+  //###################################################################################################################
+  //HELPER ############################################################################################################
+  //###################################################################################################################
 
   /*
   * alert Object and seconds to display the alert as input params
   * produces alert for x seconds dsiplayed on the right side of the management tab
   */
-  addAlertForXSeconds(alert:Alert, seconds:number)
-  {
+  addAlertForXSeconds(alert: Alert, seconds: number) {
     this.alerts.push(alert);
-    setTimeout(()=>this.alerts=this.alerts.filter(entry=>entry!=alert),seconds*1000);
+    setTimeout(() => this.alerts = this.alerts.filter(entry => entry != alert), seconds * 1000);
   }
 
-  validateDate()
-  {
+  /*
+  * function without input params
+  * checks if dates are neither in the past nor the endtime is before the starttime
+  */
+  validateDate() {
     // console.log(this.startDate)
     // console.log(new Date(this.startDate+"T00:00:00"))
     // console.log(new Date(this.startDate+"T00:00:00").getTime())
     // console.log(parseDate(this.endDate).getMilliseconds())
 
-    if(this.startTime&&this.startDate&&this.endDate&&this.endTime)
-    {
-      this.startTimestamp=parseDate(this.startDate+"T"+this.startTime)
-      this.endTimestamp=parseDate(this.endDate+"T"+this.endTime)
+    if (this.startTime && this.startDate && this.endDate && this.endTime) {
+      this.startTimestamp = parseDate(this.startDate + "T" + this.startTime)
+      this.endTimestamp = parseDate(this.endDate + "T" + this.endTime)
 
       this.startDateError = this.startTimestamp.getTime() <= this.today.getTime();
 
@@ -115,44 +121,102 @@ export class BookingManagementComponent implements OnInit {
 
   }
 
-  getPricePerUnit(_callback:Function)
-  {
-    this.roomService.getRoom(this.roomNo).subscribe(data=>
-    {
-      this.pricePerUnit=data.pricePerUnit;
+  /*
+  * function with callback function as input param for a controlled function sequence
+  * retrieves pricePerUnit associated with roomNo in current form data
+  */
+  getPricePerUnit(_callback: Function) {
+    this.roomService.getRoom(this.roomNo).subscribe(data => {
+      this.pricePerUnit = data.pricePerUnit;
       _callback();
     })
   }
 
-  calculatePricing(_callback:Function)
-  {
-    let timeDifferenceInMilliseconds=this.endTimestamp.getTime()-this.startTimestamp.getTime()
-    let divisorToConvertFromMSInDays=1000 * 60 * 60 * 24;
-    let divisorToConvertFromMSInHours=60 * 60 * 1000;
+  /*
+  * function with callback function as input param for a controlled function sequence
+  * calculates the pricing depending on the time period given in form data and the pricePerUnit of the wanted room
+  */
+  calculatePricing(_callback: Function) {
+    let timeDifferenceInMilliseconds = this.endTimestamp.getTime() - this.startTimestamp.getTime()
+    let divisorToConvertFromMSInDays = 1000 * 60 * 60 * 24;
+    let divisorToConvertFromMSInHours = 60 * 60 * 1000;
 
-    if(this.bookingType==="hotelRoom")
-    {
-      let pricePerNight:number;
-      pricePerNight=this.pricePerUnit
+    if (this.bookingType === "hotelRoom") {
+      let pricePerNight: number;
+      pricePerNight = this.pricePerUnit
 
-      let timeDifferenceInDays=Math.floor(timeDifferenceInMilliseconds / divisorToConvertFromMSInDays);
-      if (timeDifferenceInDays==0) timeDifferenceInDays=1;
-      this.calculatedPricing=timeDifferenceInDays*pricePerNight;
+      let timeDifferenceInDays = Math.floor(timeDifferenceInMilliseconds / divisorToConvertFromMSInDays);
+      if (timeDifferenceInDays == 0) timeDifferenceInDays = 1;
+      this.calculatedPricing = timeDifferenceInDays * pricePerNight;
       _callback();
-    }
-    else
-    {
-      let pricePerHour:number;
-      pricePerHour=this.pricePerUnit
+    } else {
+      let pricePerHour: number;
+      pricePerHour = this.pricePerUnit
 
-      let timeDifferenceInHours=Math.ceil(timeDifferenceInMilliseconds/divisorToConvertFromMSInHours);
-      this.calculatedPricing=timeDifferenceInHours*pricePerHour;
+      let timeDifferenceInHours = Math.ceil(timeDifferenceInMilliseconds / divisorToConvertFromMSInHours);
+      this.calculatedPricing = timeDifferenceInHours * pricePerHour;
       _callback();
     }
   }
 
-  prepareAndInsertBooking(addsNewBooking:boolean,addOrUpdateBookingForm:NgForm)
-  {
+  /*
+  * boolean as input decides if the current chosen roomNo should be displayed (if booking should be changed) or not
+  * filters roomNos of rooms that are already occupied during parts of or the whole time period wished to be booked
+  */
+  filterRoomsByOccupation(isBookingChange: boolean) {
+    console.log(this.startDate, this.endDate);
+    let startTimestamp = this.startDate + "T" + this.startTime + ":00.000%2b02:00"
+    let endTimestamp = this.endDate + "T" + this.endTime + ":00.000%2b02:00"
+
+    this.bookingService.getBookingsByStartDateAndEndDate(startTimestamp, endTimestamp).subscribe(data => {
+      console.log(this.rooms);
+      data.forEach((booking: Booking) => {
+        if (isBookingChange) this.rooms = this.rooms.filter(room => room.roomNo !== booking.roomNo || room.roomNo === this.roomNo)
+        else this.rooms = this.rooms.filter(room => room.roomNo !== booking.roomNo)
+      })
+      console.log(this.rooms);
+      if (!isBookingChange) this.occupationChecked = true;
+    })
+  }
+
+  /*
+  * function with callback function as input param for a controlled function sequence and boolean for call of filterRoomsByOccupation
+  * gets all rooms valid for the chosen bookingType
+  */
+  getValidRooms(isBookingChange: boolean, _callback?: Function) {
+    if (this.bookingType == "hotelRoom") {
+      this.roomService.getHotelRooms().subscribe(data => {
+        console.log(data);
+        this.rooms = data;
+        console.log(this.rooms);
+        this.filterRoomsByOccupation(isBookingChange);
+        if (_callback) _callback();
+
+      })
+    } else {
+      this.roomService.getConferenceRooms().subscribe(data => {
+        console.log(data);
+        this.rooms = data;
+        console.log(this.rooms);
+        this.filterRoomsByOccupation(isBookingChange);
+        if (_callback) _callback();
+      })
+    }
+
+  }
+
+  //###################################################################################################################
+  //ADD | UPDATE ######################################################################################################
+  //###################################################################################################################
+
+  /*
+  * function with boolean and ngForm as input params
+  * boolean decides if a Booking is added or an existing Booking is updated
+  * ngForm for form resetting after update/add
+  *
+  * adds a new Booking or updates an existing booking according to form data
+  */
+  prepareAndInsertBooking(addsNewBooking: boolean, addOrUpdateBookingForm: NgForm) {
     let newOrUpdatedBooking: Booking =
       {
         customerID: this.customerID,
@@ -164,174 +228,135 @@ export class BookingManagementComponent implements OnInit {
         specialWishes: this.specialWishes,
       };
 
-    if (addsNewBooking)
-    {
+    if (addsNewBooking) {
       this.bookingService.save(newOrUpdatedBooking, this.bookingType)
-        .subscribe((data) =>
-          {
+        .subscribe((data) => {
             this.addAlertForXSeconds(new Alert('success', "Buchung erfolgreich angelegt"), 5);
             addOrUpdateBookingForm.resetForm();
           },
-          (error) =>
-          {
+          (error) => {
             this.addAlertForXSeconds(new Alert('danger', "Fehler beim Anlegen der Buchung"), 5);
           });
-    }
-    else
-    {
+    } else {
       this.bookingService.updateBooking(this.bookingNo, newOrUpdatedBooking)
-        .subscribe((data) =>
-          {
+        .subscribe((data) => {
             this.addAlertForXSeconds(new Alert('success', "Buchung erfolgreich geändert"), 5);
             addOrUpdateBookingForm.resetForm();
             this.foundBooking = null;
           },
-          (error) =>
-          {
+          (error) => {
             this.addAlertForXSeconds(new Alert('danger', "Fehler beim Ändern der Buchung"), 5);
           });
     }
   }
 
-
-
-  addOrUpdateBooking(addsNewBooking:boolean, addOrUpdateBookingForm:NgForm)
-  {
-    if (this.roleService.checkRights(this.department))
-    {
-      this.startTimestamp=parseDate(this.startDate+"T"+this.startTime)
-      this.endTimestamp=parseDate(this.endDate+"T"+this.endTime)
-      this.getPricePerUnit(()=>this.calculatePricing(()=>this.prepareAndInsertBooking(addsNewBooking,addOrUpdateBookingForm)))
-    }
-    else alert("Benötigte Rechte nicht vorhanden")
+  /*
+  * function with boolean and ngForm as input params
+  * boolean decides if a Booking is added or an existing Booking is updated
+  * ngForm for form resetting after update/add
+  *
+  * checks for employees rights to do this transaction
+  * calls  getPricePerUnit to start the add/update function sequence
+  */
+  addOrUpdateBooking(addsNewBooking: boolean, addOrUpdateBookingForm: NgForm) {
+    if (this.roleService.checkRights(this.department)) {
+      this.startTimestamp = parseDate(this.startDate + "T" + this.startTime)
+      this.endTimestamp = parseDate(this.endDate + "T" + this.endTime)
+      this.getPricePerUnit(() => this.calculatePricing(() => this.prepareAndInsertBooking(addsNewBooking, addOrUpdateBookingForm)))
+    } else alert("Benötigte Rechte nicht vorhanden")
   }
 
-  filterRoomsByOccupation(isBookingChange:boolean)
-  {
-    console.log(this.startDate, this.endDate);
-    let startTimestamp=this.startDate+"T"+this.startTime+":00.000%2b02:00"
-    let endTimestamp=this.endDate+"T"+this.endTime+":00.000%2b02:00"
+  //###################################################################################################################
+  //GET ###############################################################################################################
+  //###################################################################################################################
 
-    this.bookingService.getBookingsByStartDateAndEndDate(startTimestamp, endTimestamp).subscribe(data=>
-    {
-      console.log(this.rooms);
-      data.forEach((booking:Booking) =>
-      {
-        if(isBookingChange) this.rooms=this.rooms.filter(room=> room.roomNo!==booking.roomNo || room.roomNo===this.roomNo)
-        else this.rooms=this.rooms.filter(room=> room.roomNo!==booking.roomNo)
-      })
-      console.log(this.rooms);
-      if(!isBookingChange) this.occupationChecked=true;
-    })
+  /*
+  * function with boolean as input param
+  * boolean decides if search result should be filled into the form
+  * starts search function sequence by calling getBookingType
+  */
+  submitSearch(intoFormular: boolean) {
+    if (this.roleService.checkRights(this.department)) {
+      this.foundBooking = null;
+      this.getBookingType(() => this.getValidRooms(intoFormular, () => this.getBookingData(intoFormular)))
+    } else alert("Benötigte Rechte nicht vorhanden")
   }
 
-  getValidRooms(isBookingChange: boolean, _callback?: Function)
-  {
-    if (this.bookingType=="hotelRoom")
-    {
-      this.roomService.getHotelRooms().subscribe(data=> {
-        console.log(data);
-        this.rooms=data;
-        console.log(this.rooms);
-        this.filterRoomsByOccupation(isBookingChange);
-        if(_callback) _callback();
-
-      })
-    }
-    else
-    {
-      this.roomService.getConferenceRooms().subscribe(data=> {
-        console.log(data);
-        this.rooms=data;
-        console.log(this.rooms);
-        this.filterRoomsByOccupation(isBookingChange);
-        if(_callback) _callback();
-      })
-    }
-
-  }
-
-  submitSearch(intoFormular:boolean)
-  {
-    if (this.roleService.checkRights(this.department))
-    {
-      this.foundBooking=null;
-      this.getBookingType(()=>this.getValidRooms(intoFormular, () => this.getBookingData(intoFormular)))
-    }
-    else alert("Benötigte Rechte nicht vorhanden")
-  }
-
-  getBookingType(_callback:Function)
-  {
-    this.bookingService.getBooking(this.bookingNo).subscribe(data=>
-    {
-      if(data._links?.hotelRoomBooking) this.bookingType="hotelRoom";
-      else if(data._links?.conferenceRoomBooking) this.bookingType="conferenceRoom";
-      _callback();
-    },
-      (error)=>
-      {
-        this.addAlertForXSeconds(new Alert('danger',"Keine Buchungen zu dieser Buchungsnummer vorhanden"),5);
+  /*
+  * function with callback function as input param for controlled function sequence
+  * retrieves correct bookingType for the given bookingNo and calls getBookingData afterwards
+  */
+  getBookingType(_callback: Function) {
+    this.bookingService.getBooking(this.bookingNo).subscribe(data => {
+        if (data._links?.hotelRoomBooking) this.bookingType = "hotelRoom";
+        else if (data._links?.conferenceRoomBooking) this.bookingType = "conferenceRoom";
+        _callback();
+      },
+      (error) => {
+        this.addAlertForXSeconds(new Alert('danger', "Keine Buchungen zu dieser Buchungsnummer vorhanden"), 5);
 
       })
   }
 
-  getBookingData(intoFormular:boolean)
-  {
+  /*
+  * function with boolean as input param
+  * boolean decides if search result should be filled into the form
+  * searches for employee associated with the form data
+  */
+  getBookingData(intoFormular: boolean) {
 
-    this.foundBooking=null;
+    this.foundBooking = null;
     console.log(this.rooms);
 
-    this.bookingService.getBooking(this.bookingNo).subscribe(data=>
-    {
-      this.foundBooking=data;
-      this.foundBooking.startDate=formatDate(data.startDate,"yyyy-MM-dd HH:mm:ss","de");
-      this.foundBooking.endDate=formatDate(data.endDate,"yyyy-MM-dd HH:mm:ss","de");
+    this.bookingService.getBooking(this.bookingNo).subscribe(data => {
+        this.foundBooking = data;
+        this.foundBooking.startDate = formatDate(data.startDate, "yyyy-MM-dd HH:mm:ss", "de");
+        this.foundBooking.endDate = formatDate(data.endDate, "yyyy-MM-dd HH:mm:ss", "de");
 
-      if(intoFormular)
-      {
-        data.startDate=formatDate(data.startDate,"yyyy-MM-dd HH:mm:ss","de");
-        data.endDate=formatDate(data.endDate,"yyyy-MM-dd HH:mm:ss","de");
-        this.customerID=data.customerID;
-        this.roomNo=data.roomNo;
-        this.startDate=formatDate(data.startDate,"yyyy-MM-dd","de");
-        this.startTime=formatDate(data.startDate,"HH:mm","de");
-        this.endDate=formatDate(data.endDate,"yyyy-MM-dd","de");
-        this.endTime=formatDate(data.endDate,"HH:mm","de");
-        this.specialWishes=data.specialWishes
-        this.filterRoomsByOccupation(true);
-      }
+        if (intoFormular) {
+          data.startDate = formatDate(data.startDate, "yyyy-MM-dd HH:mm:ss", "de");
+          data.endDate = formatDate(data.endDate, "yyyy-MM-dd HH:mm:ss", "de");
+          this.customerID = data.customerID;
+          this.roomNo = data.roomNo;
+          this.startDate = formatDate(data.startDate, "yyyy-MM-dd", "de");
+          this.startTime = formatDate(data.startDate, "HH:mm", "de");
+          this.endDate = formatDate(data.endDate, "yyyy-MM-dd", "de");
+          this.endTime = formatDate(data.endDate, "HH:mm", "de");
+          this.specialWishes = data.specialWishes
+          this.filterRoomsByOccupation(true);
+        }
 
-    },
-      (error)=>
-      {
-        this.addAlertForXSeconds(new Alert('danger',"Keine Buchungen zu dieser Buchungsnummer vorhanden"),5);
+      },
+      (error) => {
+        this.addAlertForXSeconds(new Alert('danger', "Keine Buchungen zu dieser Buchungsnummer vorhanden"), 5);
       });
     return;
   }
 
+  //###################################################################################################################
+  //DELETE ############################################################################################################
+  //###################################################################################################################
 
-
-  deleteBooking(deleteBookingForm:NgForm)
-  {
-    if (this.roleService.checkRights(this.department))
-    {
+  /*
+  * ngForm as input param
+  * ngForm for resetting the form after deletion
+  * deletes booking associated with the form data
+  */
+  deleteBooking(deleteBookingForm: NgForm) {
+    if (this.roleService.checkRights(this.department)) {
       this.bookingService.delete(this.bookingNo)
         .subscribe(
-          (data)=>
-          {
-            this.addAlertForXSeconds(new Alert('success',"Buchung erfolgreich gelöscht"),5);
+          (data) => {
+            this.addAlertForXSeconds(new Alert('success', "Buchung erfolgreich gelöscht"), 5);
             deleteBookingForm.resetForm()
-            this.foundBooking=null;
+            this.foundBooking = null;
           },
 
-        (error)=>
-        {
-          this.addAlertForXSeconds(new Alert('danger',"Fehler beim Löschen der Buchung"),5);
-        }
-          )
-    }
-    else alert("Benötigte Rechte nicht vorhanden")
+          (error) => {
+            this.addAlertForXSeconds(new Alert('danger', "Fehler beim Löschen der Buchung"), 5);
+          }
+        )
+    } else alert("Benötigte Rechte nicht vorhanden")
   }
 
 }
