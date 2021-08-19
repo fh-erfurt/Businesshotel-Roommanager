84385d623c71c53d542f7a001056c4cafe263977
Thom-Mon <thomas.gebel@fh-erfurt.de>
2021-08-19 11:15:04 +0200

    Kommentare in den Repos ergänzt

--- src/main/java/de/fourofakind/businesshotel/server/repositories/ConferenceRoomRepository.java
+++ src/main/java/de/fourofakind/businesshotel/server/repositories/ConferenceRoomRepository.java
@@ -4,7 +4,10 @@ import de.fourofakind.businesshotel.server.entities.rooms.ConferenceRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
+/**
+ * <p>This repository represents the detailed specified information of an conferenceRoom. There are certain information that are necessary
+ * for the conferenceRoom like the amount of beamers or screens. These information are only relevant for conferencerooms and its users.</p>
+ */
 @CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
 @RepositoryRestResource(collectionResourceRel = "conferenceroom", path="conferenceroom")
 public interface ConferenceRoomRepository extends JpaRepository<ConferenceRoom,Integer>
--- src/main/java/de/fourofakind/businesshotel/server/repositories/CustomerRepository.java
+++ src/main/java/de/fourofakind/businesshotel/server/repositories/CustomerRepository.java
@@ -9,7 +9,10 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
-
+/**
+ * <p>This repository represents the Customer. It contains the payment-method and refers the customer to his contactdata and his account.
+ * Furthermore it holds the information whether the customer is a business-customer or not.</p>
+ */
 @CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
 @RepositoryRestResource(collectionResourceRel = "customer", path="customer")
 public interface CustomerRepository extends JpaRepository<Customer,Integer>
